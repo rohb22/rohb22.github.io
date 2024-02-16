af70c707c55b4d39f98bf6db29048f141a9cab3a
John Rohb Paloma <palomajohnrohb@gmail.com>
2024-02-16 19:18:19 +0800

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ export default function Navbar() {
         <div id="navbar">
             <ul id="navbar-items">
                 <li className="navbar-item"><a href="#about-container">About</a></li>
-                <li className="navbar-item">Projects</li>
-                <li className="navbar-item">Contact</li>
+                <li className="navbar-item"><a href="#projects-container">Projects</a></li>
+                <li className="navbar-item"><a href="#contact-div">Contacts</a></li>
             </ul>
         </div>
     )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Add the base option to specify the base URL
   plugins: [react()],
-})
+});
