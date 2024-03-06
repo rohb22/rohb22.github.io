1d1f1ff8a97d8f47a2fb09d417fcf4d1c0189152
John Rohb Paloma <palomajohnrohb@gmail.com>
2024-03-06 09:26:42 +0800

    Use paths on a json file insted of importing each one

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,10 @@
 import React from "react"
-import PyhtonIcon from "../assets/icons/python.svg"
-import CppIcon from "../assets/icons/cpp.svg"
-import CIcon from "../assets/icons/c.svg"
-import ReactIcon from "../assets/icons/react.svg"
-import JavaIcon from "../assets/icons/java.svg"
-import JsIcon from "../assets/icons/javascript.svg"
-import HtmlIcon from "../assets/icons/html5.svg"
-import CssIcon from "../assets/icons/css3.svg"
-import GitIcon from "../assets/icons/git.svg"
+import skillsdb from "../skills.json"
 
 function Skill({icon, name}) {
     return (
         <div className="icon-container">
-            <img className="icons" src={icon} alt={`${name} icon`} />
+            <img className="icons" src={require(icon)} alt={`${name} icon`} />
             <p className="icons-name">{name}</p>
         </div>
     )
@@ -20,19 +12,14 @@ function Skill({icon, name}) {
 
 
 export default function Skills() {
+    
     return (
         <div id="skills-container">
             <h3>My Tech Stack</h3>
             <div id="skills">
-                <Skill icon={PyhtonIcon} name="Python"/>
-                <Skill icon={CppIcon} name="C++"/>
-                <Skill icon={CIcon} name="C"/>
-                <Skill icon={JavaIcon} name="Java"/>
-                <Skill icon={ReactIcon} name="React"/>
-                <Skill icon={HtmlIcon} name="Html"/>
-                <Skill icon={CssIcon} name="Css"/>
-                <Skill icon={JsIcon} name="Javascript"/>
-                <Skill icon={GitIcon} name="Git/Github"/>                
+                {skillsdb.skills.map((item, i) => (
+                    <Skill icon={item.path} name={item.name} key={i}/>
+                ))}
             </div>
         </div>
     )
