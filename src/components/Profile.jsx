1c1af96744b2e1d146059c9cecaf7a610c5d0d21
rohb22 <rohbpaloma@gmail.com>
2024-08-19 12:37:38 +0800

    will be fixing styling

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Photo from "../assets/profile.png";
 
 
 export default function Profile() {
     return (
         <div id="profile-container">
-            <img src={Photo} id="profilephoto"/>
             <div id="profile-content">
                 <h3>I'm <span id="profilename">Rohb,</span></h3>
                 <p>A Computer Engineering</p>
