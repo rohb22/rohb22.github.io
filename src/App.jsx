354e529668cf0ab44a8d6f330e5d434ffed8b444
rohb22 <rohbpaloma@gmail.com>
2024-08-21 20:00:41 +0800

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import './index.scss';
 import Profile from "./components/Profile";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
     <>
     <Profile />
     <Skills />
+    <Projects />
     </>
   )
 }
