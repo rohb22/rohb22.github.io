b53524567b740737c08541f2cd70e9c9b6354e11
rohb <palomajohnrohb@gmail.com>
2025-05-31 12:59:34 +0800

    reset

--- README.md
+++
@@ -1,4 +0,0 @@
-# Portfolio
-A web portfolio containing my information, including my tech stack, education and most importantly projects
-
-
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: './', // Add the base option to specify the base URL
-  plugins: [react()],
-});
