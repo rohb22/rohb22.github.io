5194894b857c8c1cf4903ad57a9ad2c85a28a7f7
rohb <palomajohnrohb@gmail.com>
2025-05-31 13:47:28 +0800

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/rohb22.github.io",
+  base: "/",
   plugins: [tailwindcss(), react()],
 });
